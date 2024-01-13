514e8482a264e2baf3d4f8eebc8a01c0be9fd84d
Tivotal <121335284+Tivotal@users.noreply.github.com>
2024-01-13 08:04:16 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+/* Created by Tivotal */
+
+let display = document.querySelector(".display");
+
+function appendToDisplay(inputValue) {
+  display.value += inputValue;
+}
+
+function backSpace() {
+  display.value = display.value.slice(0, -1);
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  display.value = eval(display.value);
+}
